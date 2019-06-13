19f7c44e198fd0e269eebe09c8e49a7660d7227a
Orlando <zn.orlando@gmail.com>
2019-06-13 11:26:00 -0300

    Metodo Constutor

---
+++ Criando uma Classe/Metodos-Magicos.php
@@ -0,0 +1,22 @@
+<?php
+
+class Endereco {
+
+private $logradoro;
+private $numero;
+private $cidade;
+
+public function __construct($a, $b, $c){
+
+    $this->logradoro = $a;
+    $this->numero = $b;
+    $this->cidade = $c;
+
+}
+
+}
+
+
+
+
+?>
